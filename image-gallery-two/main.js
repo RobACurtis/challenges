79a313b7209f57e10bd904ac1b492fcb413ad6bb
Rob Curtis <Robanthonycurtis@gmail.com>
2022-05-11 03:58:41 +0000

    added gallery

--- image-gallery-two/main.js
+++ image-gallery-two/main.js
@@ -1,12 +1,20 @@
-const $div = document.querySelector('.grid');
-const arr = ['surfing', 'sports', 'exercise', 'happy', 'love', 'joy', 'weather', 'mountains', 'skateboard', 'kids', 'snow'];
-function createImageGallery() {
-  for (var i = 0; i < arr.length; i++) {
-    const img = document.createElement('img');
-    img.setAttribute('src','https://source.unsplash.com/1600x900/?' + arr[i]);
-    img.className = 'grid-item';
-    $div.appendChild(img);
+const $div = document.querySelector('.column');
+const arr = ['/900x1600/?surfing', '/1600x900/?surfing', '/1600x900/?sports', '/900x1600/?exercise', '/1600x900/?happy', '/900x1600/?love', '/1600x900/?joy', '/900x1600/?weather', '/900x1600/?mountains', '/1600x900/?skateboard', '/900x1600/?kids', '/1600x900/?snow'];
+
+function newImage() {
+  for (var i =0; i < arr.length; i++) {
+    const img = new Image();
+    img.src = 'https://source.unsplash.com' + arr[i];
+img.onload = function () {
+      if (this.width > this.height) {
+        img.className = 'grid-item grid-item--width2';
+        $div.appendChild(img);
+      } else {
+        img.className = 'grid-item grid-item--width2';
+        $div.appendChild(img);
+      }
+    }
   }
 }
 
-createImageGallery();
+newImage();
