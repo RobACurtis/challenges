aad9cd572d07ae2304795e2811b925e02a2035d2
Rob Curtis <Robanthonycurtis@gmail.com>
2022-06-25 00:10:27 +0000

    completed string 1

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,1 +1,6 @@
 /* exported getWords */
+function getWords(string) {
+  if (!string[0]) return [];
+  const words = string.split(' ');
+  return words;
+}
--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,1 +1,8 @@
 /* exported reverseWord */
+function reverseWord(word) {
+  let reverseWord = word[word.length - 1]
+  for (let i = word.length - 2; i >= 0; i--) {
+      reverseWord += word[i];
+  }
+  return reverseWord;
+}
