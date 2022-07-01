7aa09c33b6c6ef08f0d0ee330806488d8846962f
Rob Curtis <Robanthonycurtis@gmail.com>
2022-07-01 20:52:39 +0000

    completed object manipulation 1

--- algorithm-one/object-manipulation-1/src/to-object.js
+++ algorithm-one/object-manipulation-1/src/to-object.js
@@ -1,6 +1,2 @@
 /* exported toObject */
-const toObject = keyValuePair => {
-  let obj = {}
-  obj[keyValuePair[0]] = keyValuePair[1];
-  return obj;
-} ;
+const toObject = keyValuePair => ({[keyValuePair[0]]: keyValuePair[1]});
