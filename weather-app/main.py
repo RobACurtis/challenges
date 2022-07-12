75e3159f6e2892fdb12467641defb9f4afb4b748
Rob Anthony Curtis <Robanthonycurtis@gmail.com>
2022-07-11 20:21:29 -0700

    Update main.py

--- weather-app/main.py
+++ weather-app/main.py
@@ -1,7 +1,7 @@
 import psycopg2
 import requests
 
-response = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&appid=627737b1b2bf5363884b298593e775f1")
+response = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&appid=apikey")
 weather = response.json()
 print(weather['daily'][0]['weather'])
 
