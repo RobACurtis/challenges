a4a49c3101ade2851a74eac1f5d19001011165f7
Rob Curtis <Robanthonycurtis@gmail.com>
2022-07-20 17:50:24 +0000

    completed strings-one python

---
+++ algorithm-one/string-manipulation-1/python/is-upper-cased.py
@@ -0,0 +1,13 @@
+def isUpperCased(word):
+  if (word == word.upper()):
+    return True
+  else:
+    return False
+
+
+print(isUpperCased('Salmon'))
+print(isUpperCased('LearningFuze'))
+print(isUpperCased('css'))
+print(isUpperCased('HTML'))
+print(isUpperCased('PHP'))
+print(isUpperCased('rob'))
